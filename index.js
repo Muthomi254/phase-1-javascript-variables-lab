6b70a8f1bb93ae5988115ccb7ec07e46a746ccb7
muthomi254 <dmunene202020@gmail.com>
2023-09-18 17:12:19 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+let companyName = "Scuber";
+function companyName (){
+return "is set as Scuber,{$companyName}"
+}
+
+// const companyName = "is defined as a const"
+// function js (){
+// return "is defined as a const:";
+// }
+
+let mostProfitableNeighborhood = "Chelsea"
+function mostProfitableNeighborhood (){
+ return 'is declared as equal to Chelsea'
+} 
+
+let companyCeo = "Susan Smith"
+function companyCeo (){
+  return `is declared as equal to Susan Smith`
+}
